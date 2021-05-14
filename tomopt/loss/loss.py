941594cfd933e3e7678346726afedb46626eab72
GilesStrong <giles.strong@outlook.com>
2021-05-14 18:06:53 +0200

    Dev training classes

--- tomopt/loss/loss.py
+++ tomopt/loss/loss.py
@@ -11,8 +11,9 @@ class DetectorLoss(nn.Module):
         super().__init__()
         self.cost_coef = cost_coef
 
-    def forward(self, pred_x0: Tensor, pred_weights: Tensor, volume: Volume) -> Tensor:
+    def forward(self, pred_x0: Tensor, pred_weight: Tensor, volume: Volume) -> Tensor:
         true_x0 = volume.get_rad_cube()
-        pred_loss = torch.mean((pred_x0 - true_x0).pow(2) * pred_weights)  # MSE/variance
+        pred_loss = torch.mean((pred_x0 - true_x0).pow(2) * pred_weight)  # MSE/variance
         det_loss = self.cost_coef * volume.get_cost()
+        print(f"X0 comp: {pred_loss.data.item():.2f}, cost comp: {det_loss:.0f}")
         return pred_loss + det_loss
