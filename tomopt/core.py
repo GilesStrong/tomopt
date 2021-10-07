a5dcb9e6fe4d6b4e8ab175fbb919db7e7d667890
GilesStrong <giles.strong@outlook.com>
2021-10-07 15:09:34 +0200

    Adding uranium

--- tomopt/core.py
+++ tomopt/core.py
@@ -11,7 +11,7 @@ SCATTER_COEF_A = 0.0136
 
 SCATTER_COEF_B = 0.038
 
-X0 = {
+X0 = {  # Is actually X0/density "inverse scaled scattering density"
     "beryllium": 0.3528,  # m
     "carbon": 0.1932,
     "silicon": 0.0937,
@@ -19,6 +19,7 @@ X0 = {
     "iron": 0.01757,
     "copper": 0.01436,
     "lead": 0.005612,
+    "uranium": 3.141e-3
     # 'air':312.22
 }
 
