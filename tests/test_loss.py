37d0540c06728cc1e70ca81722916378d35dfb1e
GilesStrong <giles.strong@outlook.com>
2022-01-03 15:09:13 +0100

    Test AbsDetectorLoss

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,9 +1,10 @@
 from pytest_mock import mocker  # noqa F401
 
 import torch
-from torch import nn
+from torch import nn, Tensor
 
-from tomopt.optimisation import VoxelX0Loss
+from tomopt.optimisation import VoxelX0Loss, VoxelClassLoss
+from tomopt.optimisation.loss.loss import AbsDetectorLoss
 from tomopt.volume import Volume
 
 SHP = (6, 10, 10)
@@ -13,7 +14,40 @@ class MockLayer(nn.Module):
     device = torch.device("cpu")
 
 
-def test_detector_loss(mocker):  # noqa F811
+def test_abs_detector_loss(mocker):  # noqa F811
+    cost = torch.ones((1), requires_grad=True)
+    mocker.patch("tomopt.optimisation.loss.loss.Volume.get_cost", return_value=cost)
+    pred = torch.ones(SHP, requires_grad=True) / 2
+    volume = Volume(nn.ModuleList([MockLayer()]))
+
+    class DetectorLoss(AbsDetectorLoss):
+        def _get_inference_loss(self, pred: Tensor, inv_pred_weight: Tensor, volume: Volume) -> Tensor:
+            return Tensor([3])
+
+    # No target budget
+    loss_func = DetectorLoss(target_budget=None)
+    assert loss_func.sub_losses == {}
+    assert loss_func._get_budget_coef(Tensor([0])) == Tensor([0])
+
+    assert loss_func.cost_coef is None
+    loss_func._compute_cost_coef(Tensor([2]))
+    assert loss_func.cost_coef == Tensor([2])
+    assert loss_func._get_cost_loss(volume) == Tensor([0])
+
+    val = loss_func(pred, None, volume)
+    assert val == Tensor([3])
+    assert loss_func.sub_losses["error"] == 3
+    assert loss_func.sub_losses["cost"] == Tensor([0])
+
+    # With target budget
+    loss_func = DetectorLoss(target_budget=cost)
+    val = loss_func(pred, None, volume)
+    assert val == Tensor([6])
+    assert loss_func.sub_losses["error"] == 3
+    assert loss_func.sub_losses["cost"] == Tensor([3])
+
+
+def test_voxel_X0_loss(mocker):  # noqa F811
     cost = torch.ones((1), requires_grad=True)
     true = torch.ones(SHP)
     mocker.patch("tomopt.optimisation.loss.loss.Volume.get_rad_cube", return_value=true)
@@ -21,14 +55,20 @@ def test_detector_loss(mocker):  # noqa F811
     pred = torch.ones(SHP, requires_grad=True) / 2
 
     loss_func = VoxelX0Loss(target_budget=None, cost_coef=0, debug=True)
+
+    # Loss goes to zero
+    loss_val = loss_func(true, torch.ones_like(pred), Volume(nn.ModuleList([MockLayer()])))
+    assert loss_val == 0
+
+    # Decreasing variance improves loss
     loss_val = loss_func(pred, torch.ones_like(pred), Volume(nn.ModuleList([MockLayer()])))
     assert loss_val.shape == torch.Size([1])
     assert loss_val == (pred - true).pow(2).mean()
 
-    # Decreasing variance improves loss
     new_loss_val = loss_func(pred, 10 * torch.ones_like(pred), Volume(nn.ModuleList([MockLayer()])))
     assert new_loss_val < loss_val
 
+    # Include cost
     loss_func = VoxelX0Loss(target_budget=1, cost_coef=1, debug=True, steep_budget=True)
     loss_val = loss_func(pred, torch.ones_like(pred), Volume(nn.ModuleList([MockLayer()])))
     assert loss_val.shape == torch.Size([1])
