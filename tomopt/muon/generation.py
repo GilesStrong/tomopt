Fix theta calculation

tomopt/muon/generation.py
@@ -103,7 +103,7 @@ class MuonGenerator:
 
         # Get theta_x and theta_y from theta and phi
         phi = np.random.uniform(0, 2 * np.pi, n_muons)
-        theta_x = np.arcsin(np.sin(theta) * np.cos(phi))
+        theta_x = np.arcsin(np.sin(theta) * np.cos(phi))  # N.B. Theta defined from z axis
         theta_y = np.arcsin(np.sin(theta) * np.sin(phi))
 
         # Generate x and y randomly

tomopt/muon/muon_batch.py
@@ -104,10 +104,10 @@ class MuonBatch:
 
     @property
     def theta(self) -> Tensor:
-        return torch.sqrt(((self.theta_x) ** 2) + ((self.theta_y) ** 2))
+        return self.theta_xy.sin().square().sum(-1).sqrt().arcsin()
 
     @theta.setter
-    def theta(self, theta: Tensor) -> None:
+    def theta(self, theta: Tensor, phi: Tensor) -> None:
         raise NotImplementedError()
 
     def propagate(self, dz: Union[Tensor, float]) -> None: