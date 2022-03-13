Fix phi calculation

tests/test_inference.py
@@ -345,8 +345,8 @@ def test_scatter_batch_compute(mocker, voxel_scatter_batch):  # noqa F811
     sb = VoxelScatterBatch(mu=mu, volume=volume)
     assert (sb.location - Tensor([[0.0, 0.5, 0.5]])).sum().abs() < 1e-3
     assert (sb.dxy - Tensor([[0.0, 0.0]])).sum().abs() < 1e-3
-    assert (sb.phi_in).sum().abs() < 1e-3
-    assert (sb.phi_out - torch.pi).sum().abs() < 1e-3
+    assert (sb.phi_in - (torch.pi / 2)).sum().abs() < 1e-3
+    assert (sb.phi_out - (3 * torch.pi / 2)).sum().abs() < 1e-3
 
     assert (sb.theta_in - (torch.pi / 4)).sum().abs() < 1e-3
     assert (sb.theta_out - (torch.pi / 4)).sum().abs() < 1e-3
@@ -418,7 +418,7 @@ def test_abs_volume_inferer_properties(voxel_scatter_batch):
     assert inferer.size == SZ
 
 
-@pytest.mark.flaky(max_runs=3, min_passes=1)
+@pytest.mark.flaky(max_runs=4, min_passes=1)
 def test_voxel_x0_inferer_methods():
     volume = Volume(get_voxel_layers(init_res=1e3))
     gen = MuonGenerator.from_volume(volume)

tomopt/inference/scattering.py
@@ -244,7 +244,7 @@ class AbsScatterBatch(metaclass=ABCMeta):
         phi = torch.arctan(y / x)  # (-pi/2, pi/2)
         m = x < 0
         phi[m] = phi[m] + torch.pi
-        m = ((x > 0) * (y < 0)).bool()
+        m = ((x >= 0) * (y < 0)).bool()
         phi[m] = phi[m] + (2 * torch.pi)  # (0, 2pi)
         return phi
 

tomopt/muon/muon_batch.py
@@ -258,7 +258,7 @@ class MuonBatch:
         phi = torch.arctan(theta_y.tan() / theta_x.tan())  # (-pi/2, pi/2)
         m = theta_x < 0
         phi[m] = phi[m] + torch.pi
-        m = ((theta_x > 0) * (theta_y < 0)).bool()
+        m = ((theta_x >= 0) * (theta_y < 0)).bool()
         phi[m] = phi[m] + (2 * torch.pi)  # (0, 2pi)
 
         phi[(theta_x.abs() >= torch.pi / 2) + (theta_y.abs() >= torch.pi / 2)] = torch.nan