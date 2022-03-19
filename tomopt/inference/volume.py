78b8e424db3ae8de099b617a5eb5bcba1fe14df9
GilesStrong <giles.strong@outlook.com>
2022-03-19 13:18:33 +0100

    Move to new features

--- tomopt/inference/volume.py
+++ tomopt/inference/volume.py
@@ -367,7 +367,7 @@ class WeightedDeepVolumeInferer(DeepVolumeInferer):
         self.in_var_unc = torch.cat(self.in_var_uncs, dim=0)
         self.efficiency = torch.cat(self.efficiencies, dim=0)
 
-        weight = self.efficiency[:, None] / ((self.in_var_unc[:, 4:5] / self.in_var[:, 4:5]) ** 2)
+        weight = self.efficiency[:, None] / ((self.in_var_unc[:, 6:7] / self.in_var[:, 6:7]) ** 2)
         weighted_vars = torch.cat((weight, self.in_var), dim=1)
         inputs = self._build_inputs(weighted_vars)
         pred = self.model(inputs[None])
