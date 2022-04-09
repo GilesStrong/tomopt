11c5890c65197fda6825168dff508b7777252a5f
Max Lamparth <max.lamparth@tum.de>
2022-04-09 14:20:04 +0200

    style: Replaced unnecessary .T.mean() with mean(axis=0).

--- tomopt/optimisation/callbacks/monitors.py
+++ tomopt/optimisation/callbacks/monitors.py
@@ -376,8 +376,10 @@ class PanelMetricLogger(MetricLogger):
                     raise ValueError(f"Detector {det} is not a PanelDetectorLayer")
                 for p in det.panels:
                     if det.type_label == "heatmap":
-                        l.append(np.concatenate((p.mu.detach().cpu().numpy().T.mean(1), p.z.detach().cpu().numpy())))
-                        s.append(p.sig.detach().cpu().numpy().T.mean(1))
+                        l_val = np.concatenate((p.mu.detach().cpu().numpy().mean(axis=0), p.z.detach().cpu().numpy()))
+                        s_val = p.sig.detach().cpu().numpy().mean(axis=0)
+                        l.append(l_val)
+                        s.append(s_val)
                     else:
                         l.append(np.concatenate((p.xy.detach().cpu().numpy(), p.z.detach().cpu().numpy())))
                         s.append(p.xy_span.detach().cpu().numpy())
