5e81c98f779c15aeb9009979e12911c4ddc634d4
GilesStrong <giles.strong@outlook.com>
2022-04-21 13:47:50 +0200

    Dev: phidet scattering sync

--- tomopt/benchmarks/phi_detector/inference.py
+++ tomopt/benchmarks/phi_detector/inference.py
@@ -1,27 +1,94 @@
-# from typing import Tuple
+from typing import List, Tuple
 
-# import torch
-# from torch import Tensor
+import torch
+from torch import Tensor
 
 from ...inference.scattering import AbsScatterBatch
+
+from .detector import PhiDetectorPanel
 
 __all__ = ["PhiDetScatterBatch"]
 
 
 class PhiDetScatterBatch(AbsScatterBatch):
-    pass
-    # @staticmethod
-    # def get_muon_trajectory(hits: Tensor, uncs: Tensor, lw: Tensor) -> Tuple[Tensor, Tensor]:
-    #     r"""
-    #     hits = (muons,panels,(x,y,z))
-    #     uncs = (muons,panels,(unc,unc,0))
+    def _extract_hits(self) -> None:
+        # reco x, reco y, gen z, must be a list to allow computation of uncertainty
+        above_hits = torch.stack(
+            [
+                torch.cat([self.hits["above"]["reco_h"][:, i], self.hits["above"]["phi"][:, i], self.hits["above"]["z"][:, i]], dim=-1)
+                for i in range(self.hits["above"]["reco_h"].shape[1])
+            ],
+            dim=1,
+        )  # muons, panels, xyz
+        below_hits = torch.stack(
+            [
+                torch.cat([self.hits["below"]["reco_h"][:, i], self.hits["below"]["phi"][:, i], self.hits["below"]["z"][:, i]], dim=-1)
+                for i in range(self.hits["below"]["reco_h"].shape[1])
+            ],
+            dim=1,
+        )
+        _above_gen_hits = torch.stack(
+            [torch.cat([self.hits["above"]["gen_xy"][:, i], self.hits["above"]["z"][:, i]], dim=-1) for i in range(self.hits["above"]["gen_xy"].shape[1])],
+            dim=1,
+        )  # muons, panels, xyz
+        _below_gen_hits = torch.stack(
+            [torch.cat([self.hits["below"]["gen_xy"][:, i], self.hits["below"]["z"][:, i]], dim=-1) for i in range(self.hits["below"]["gen_xy"].shape[1])],
+            dim=1,
+        )
+        self._n_hits_above = above_hits.shape[1]
+        self._n_hits_below = below_hits.shape[1]
 
-    #     Assume no uncertainty for z
+        # Combine all input vars into single tensor, NB ideally would stack to new dim but can't assume same number of panels above & below
+        self._reco_hits = torch.cat((above_hits, below_hits), dim=1)  # muons, all panels, reco h,phi,z
+        self._gen_hits = torch.cat((_above_gen_hits, _below_gen_hits), dim=1)  # muons, all panels, true xyz
 
-    #     In eval mode:
-    #         Muons with <2 hits within panels have NaN trajectory.
-    #         Muons with >=2 hits in panels have valid trajectories
-    #     """
+    def plot_scatter(self, idx: int) -> None:
+        raise NotImplementedError("Ah, I see you've just volunteered to implement this!")
 
-    #     hits = torch.where(torch.isinf(hits), lw.mean().type(hits.type()) / 2, hits)
-    #     uncs = torch.nan_to_num(uncs)  # Set Infs to large number
+    @staticmethod
+    def _get_hit_uncs(zordered_panels: List[PhiDetectorPanel], hits: Tensor) -> Tensor:
+        uncs: List[Tensor] = []
+        for l, h in zip(zordered_panels, hits.unbind(1)):
+            r = 1 / l.resolution
+            uncs.append(torch.cat([r, torch.zeros((len(r), 2), device=r.device)], dim=-1))
+        return torch.stack(uncs, dim=1)  # muons, panels, unc h,phi,z, zero unc for phi and z
+
+    def _compute_tracks(self) -> None:
+        def _get_panels() -> List[PhiDetectorPanel]:
+            panels = []
+            for det in self.volume.get_detectors():
+                if not isinstance(det, PhiDetectorPanel):
+                    raise ValueError(f"Detector {det} is not a PhiDetectorPanel")
+                panels += [det.panels[j] for j in det.get_panel_zorder()]
+            return panels
+
+        self._hit_uncs = self._get_hit_uncs(_get_panels(), self.gen_hits)
+        self._track_in, self._track_start_in = self.get_muon_trajectory(self.above_hits, self.above_hit_uncs, self.volume.lw)
+        self._track_out, self._track_start_out = self.get_muon_trajectory(self.below_hits, self.below_hit_uncs, self.volume.lw)
+
+    @staticmethod
+    def get_muon_trajectory(hits: Tensor, uncs: Tensor, lw: Tensor) -> Tuple[Tensor, Tensor]:
+        r"""
+        hits = (muons,panels,(h,phi,z))
+        uncs = (muons,panels,(unc,0,0))
+
+        Assumes no uncertainty for z and phi
+
+        Uses an analytic likelihood-maximisation: L = \prod_i G[h_i - h_{i,opt}(x0,y0,theta,phi)]
+        where:
+        h_{i,opt} = x_{i,opt} cos(phi_i) + y_{i,opt} sin(phi_i)
+        x_{i,opt} = x0 + z_i tan(theta) cos(phi)
+        y_{i,opt} = y0 + z_i tan(theta) sin(phi)
+
+        In eval mode:
+            Muons with <2 hits within panels have NaN trajectory.
+            Muons with >=2 hits in panels have valid trajectories
+        """
+
+        hits = torch.where(torch.isinf(hits), lw.mean().type(hits.type()) / 2, hits)
+        uncs = torch.nan_to_num(uncs)  # Set Infs to large number
+
+        x0: Tensor
+        y0: Tensor  # Track positions at Z0=0
+        phi: Tensor
+        theta: Tensor  # Track angles
