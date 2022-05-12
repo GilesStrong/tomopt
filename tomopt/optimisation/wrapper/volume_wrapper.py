8411915337cf13e87773cb37181d228b34c1b239
GilesStrong <giles.strong@outlook.com>
2022-05-12 16:04:49 +0200

    Feat: budget weights exposed as parameters to optimiser

--- tomopt/optimisation/wrapper/volume_wrapper.py
+++ tomopt/optimisation/wrapper/volume_wrapper.py
@@ -409,6 +409,7 @@ class PanelVolumeWrapper(AbsVolumeWrapper):
         xy_pos_opt: PartialOpt,
         z_pos_opt: PartialOpt,
         xy_span_opt: PartialOpt,
+        budget_opt: Optional[PartialOpt] = None,
         loss_func: Optional[AbsDetectorLoss],
         mu_generator: Optional[AbsMuonGenerator] = None,
         partial_scatter_inferer: Type[AbsScatterBatch] = PanelScatterBatch,
@@ -416,7 +417,7 @@ class PanelVolumeWrapper(AbsVolumeWrapper):
     ):
         super().__init__(
             volume=volume,
-            partial_opts={"xy_pos_opt": xy_pos_opt, "z_pos_opt": z_pos_opt, "xy_span_opt": xy_span_opt},
+            partial_opts={"xy_pos_opt": xy_pos_opt, "z_pos_opt": z_pos_opt, "xy_span_opt": xy_span_opt, "budget_opt": budget_opt},
             loss_func=loss_func,
             mu_generator=mu_generator,
             partial_scatter_inferer=partial_scatter_inferer,
@@ -434,6 +435,10 @@ class PanelVolumeWrapper(AbsVolumeWrapper):
             "z_pos_opt": kwargs["z_pos_opt"]((p.z for l in dets for p in l.panels)),
             "xy_span_opt": kwargs["xy_span_opt"]((p.xy_span for l in dets for p in l.panels)),
         }
+        if kwargs["budget_opt"] is not None:
+            self.opts["budget_opt"] = kwargs["xy_span_opt"](
+                (o.budget_weights for o in [self.volume] + [l for l in self.volume.layers if hasattr(l, "get_cost")])
+            )
 
     @classmethod
     def from_save(
