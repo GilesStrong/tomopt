7f16835c47c5c9eb41f5df2a14e798fec03d52db
GilesStrong <giles.strong@outlook.com>
2022-06-08 10:59:09 +0200

    Feat: add weights and reduction to integer class loss

--- tomopt/optimisation/loss/sub_losses.py
+++ tomopt/optimisation/loss/sub_losses.py
@@ -1,11 +1,38 @@
+from typing import Optional
+
 import torch
 from torch import Tensor
 
 __all__ = ["integer_class_loss"]
 
 
-def integer_class_loss(int_probs: Tensor, target_int: Tensor, pred_start_int: int) -> Tensor:
+def integer_class_loss(
+    int_probs: Tensor,
+    target_int: Tensor,
+    pred_start_int: int,
+    use_mse: bool,
+    batch_weights: Optional[Tensor] = None,
+    target_weights: Optional[Tensor] = None,
+    reduction: str = "mean",
+) -> Tensor:
     ints = torch.arange(pred_start_int, pred_start_int + int_probs.size(1))
-    diffs = torch.softmax((target_int - ints).abs(), dim=-1)
+    diffs = target_int - ints
+    if use_mse:
+        diffs = diffs**2
+    else:
+        diffs = diffs.abs()
+    if target_weights is not None:
+        diffs = diffs * target_weights  # Weight classes by importance
+    diffs = torch.softmax(diffs, dim=-1)
     loss = diffs * int_probs
-    return loss.sum(-1).mean()
+    loss = loss.sum(-1)
+
+    if batch_weights is not None:
+        loss = loss * batch_weights
+
+    if reduction == "mean":
+        return loss.mean(-1)
+    elif reduction == "sum":
+        return loss.sum(-1)
+    else:
+        return loss
