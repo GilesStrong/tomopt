9ded3a8c6a1a14a916aa447c8bb2888e5982da58
GilesStrong <giles.strong@outlook.com>
2022-06-14 11:00:05 +0200

    Feat: Add LadleFurnaceIntClassLoss

--- tomopt/benchmarks/ladle_furnace/__init__.py
+++ tomopt/benchmarks/ladle_furnace/__init__.py
@@ -1,4 +1,5 @@
 from .data import *  # noqa F403
 from .inference import *  # noqa F403
+from .loss import *  # noqa F403
 
-__all__ = [*data.__all__, *inference.__all__]  # type: ignore    # noqa F405
+__all__ = [*data.__all__, *inference.__all__, *loss.__all__]  # type: ignore    # noqa F405
