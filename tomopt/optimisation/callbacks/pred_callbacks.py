b64727a0a7015921b016a0a0f18ee3d5c34ae358
GilesStrong <giles.strong@outlook.com>
2022-06-14 10:53:28 +0200

    Refactor: Generalise ClassPredHandler to handle float targets

--- tomopt/optimisation/callbacks/pred_callbacks.py
+++ tomopt/optimisation/callbacks/pred_callbacks.py
@@ -1,10 +1,10 @@
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Optional
 import numpy as np
 
 from .callback import Callback
 from ...utils import x0targs_to_classtargs
 
-__all__ = ["PredHandler", "ClassPredHandler"]
+__all__ = ["PredHandler", "VolumeTargetPredHandler"]
 
 
 class PredHandler(Callback):
@@ -24,16 +24,18 @@ class PredHandler(Callback):
             self.preds.append((self.wrapper.fit_params.pred.detach().cpu().numpy(), self.wrapper.volume.get_rad_cube().detach().cpu().numpy()))
 
 
-class ClassPredHandler(PredHandler):
+class VolumeTargetPredHandler(PredHandler):
     r"""
     :class:`.PredHandler` which returns the volume target as the target value, rather than the voxel-wise X0s.
+    If a x02id lookup is provided, it transforms the target from an X0 value to a material class ID.
     """
 
-    def __init__(self, x02id: Dict[float, int]):
+    def __init__(self, x02id: Optional[Dict[float, int]]):
         self.x02id = x02id
 
     def on_x0_pred_end(self) -> None:
         if self.wrapper.fit_params.state == "test":
             targ = self.wrapper.volume.target.detach().cpu().numpy()
-            targ = x0targs_to_classtargs(targ, self.x02id)
+            if self.x02id is not None:
+                targ = x0targs_to_classtargs(targ, self.x02id)
             self.preds.append((self.wrapper.fit_params.pred.detach().cpu().numpy(), targ))
