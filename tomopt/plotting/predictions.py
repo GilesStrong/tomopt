240b0c45675f494335931a5964f33c159c1ef735
GilesStrong <giles.strong@outlook.com>
2022-10-26 15:38:24 +0200

    Docs: predictions

--- tomopt/plotting/predictions.py
+++ tomopt/plotting/predictions.py
@@ -9,6 +9,17 @@ __all__ = ["plot_pred_true_x0"]
 
 
 def plot_pred_true_x0(pred: np.ndarray, true: np.ndarray, savename: Optional[str] = None) -> None:
+    r"""
+    Plots the predicted voxelwise X0s compared to the true values of the X0s.
+    2D plots are produced in xy for layers in z in order of increasing z, i.e. the bottom most layer is the first to be plotted.
+    TODO: revise this ordering to make it more intuitive
+
+    Arguments:
+        pred: (z,x,y) array of predicted X0s
+        true: (z,x,y) array of true X0s
+        savename: optional savename for saving the plot
+    """
+
     with sns.axes_style(style="whitegrid", rc={"patch.edgecolor": "none"}):
         fig, axs = plt.subplots(len(pred), 2, figsize=(H_MID, W_MID))
         pred_cbar_ax = fig.add_axes([0.45, 0.25, 0.03, 0.5])
