Fix: swap sign of dxyz_vol decomposition

tomopt/volume/layer.py
@@ -280,9 +280,9 @@ class PassiveLayer(AbsLayer):
         # (generation of MSC formulas are oblivious of angle of incidence) so we need
         # to decompose them into displacements in x,y,z in the volume frame
         phi_defined = theta != 0  # If theta is a zero, there is no phi defined
-        dx_vol = torch.where(phi_defined, (dxy_mu[1] * torch.cos(theta) * torch.cos(phi)) + (dxy_mu[0] * torch.sin(phi)), dxy_mu[0])
-        dy_vol = torch.where(phi_defined, (dxy_mu[1] * torch.cos(theta) * torch.sin(phi)) - (dxy_mu[0] * torch.cos(phi)), dxy_mu[1])
-        dz_vol = torch.where(phi_defined, dxy_mu[1] * torch.sin(theta), dxy_mu.new_zeros(1))
+        dx_vol = torch.where(phi_defined, (dxy_mu[0] * torch.sin(phi)) + (dxy_mu[1] * torch.cos(theta) * torch.cos(phi)), dxy_mu[0])
+        dy_vol = torch.where(phi_defined, (dxy_mu[0] * torch.cos(phi)) - (dxy_mu[1] * torch.cos(theta) * torch.sin(phi)), dxy_mu[1])
+        dz_vol = torch.where(phi_defined, dxy_mu[1] * torch.sin(theta), torch.zeros_like(dxy_mu[1]))
         return {"dtheta_x_vol": dtheta_xy_mu[0], "dtheta_y_vol": dtheta_xy_mu[1], "dx_vol": dx_vol, "dy_vol": dy_vol, "dz_vol": dz_vol}
 
     def _compute_scattering(