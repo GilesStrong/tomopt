17a697462aed4d970fd2a1c26899e79100a6931a
GilesStrong <giles.strong@outlook.com>
2024-02-10 14:55:29 +0900

    Feat: Add detectors

--- tomopt/benchmarks/ladle_furnace/__init__.py
+++ tomopt/benchmarks/ladle_furnace/__init__.py
@@ -1,5 +1,6 @@
 from .data import *  # noqa F403
 from .inference import *  # noqa F403
 from .loss import *  # noqa F403
+from .volume import *  # noqa F403
 
-__all__ = [*data.__all__, *inference.__all__, *loss.__all__]  # type: ignore    # noqa F405
+__all__ = [*data.__all__, *inference.__all__, *loss.__all__, *volume.__all__]  # type: ignore    # noqa F405
