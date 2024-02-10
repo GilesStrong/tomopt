9492c9dd397277e0f89c9b43389fa804a4c601b6
GilesStrong <giles.strong@outlook.com>
2024-02-10 14:16:40 +0900

    Feat: Add spread range loss

--- tomopt/benchmarks/ladle_furnace/loss.py
+++ tomopt/benchmarks/ladle_furnace/loss.py
@@ -1,12 +1,14 @@
-from typing import Optional, Union
+from collections import defaultdict
+from typing import Dict, List, Optional, Union
 
 import torch
 from torch import Tensor
 
+from ...optimisation.callbacks import Callback
 from ...optimisation.loss import VolumeIntClassLoss
 from ...volume import Volume
 
-__all__ = ["LadleFurnaceIntClassLoss"]
+__all__ = ["LadleFurnaceIntClassLoss", "SpreadRangeLoss"]
 
 
 class LadleFurnaceIntClassLoss(VolumeIntClassLoss):
@@ -41,3 +43,28 @@ class LadleFurnaceIntClassLoss(VolumeIntClassLoss):
         return (
             torch.div((targs - volume.get_passive_z_range()[0]), volume.passive_size, rounding_mode="floor") - 1
         )  # -1 due to conversion to layer ID, instead of fill height
+
+
+class SpreadRangeLoss(Callback):
+    def on_volume_batch_begin(self) -> None:
+        self._preds: Dict[float, List[Tensor]] = defaultdict(list)
+
+    def on_x0_pred_end(self) -> None:
+        self._preds[self.wrapper.volume.target.cpu().item()].append(self.wrapper.fit_params.pred)
+
+    def on_volume_batch_end(self) -> None:
+        stds = []
+        means = []
+        for preds in self._preds.values():
+            if len(preds) < 2:
+                continue
+            p = torch.cat(preds)
+            stds.append(torch.std(p))
+            means.append(torch.mean(p))
+
+        spread = torch.mean(torch.stack(stds))
+        range_ = torch.std(torch.stack(means))
+
+        loss = spread / range_
+
+        self.wrapper.fit_params.mean_loss = loss[None]
