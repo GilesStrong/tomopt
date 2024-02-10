c1101f29a6f9a1db67bd7fb54753fa7426c06e21
GilesStrong <giles.strong@outlook.com>
2024-02-10 15:19:46 +0900

    Feat: Add result plots

--- tomopt/plotting/__init__.py
+++ tomopt/plotting/__init__.py
@@ -1,4 +1,5 @@
+from .appearance import *  # noqa F403
 from .diagnostics import *  # noqa F403
 from .predictions import *  # noqa F403
 
-__all__ = [*predictions.__all__, *diagnostics.__all__]  # type: ignore  # noqa F405
+__all__ = [*predictions.__all__, *diagnostics.__all__, *appearance.__all_]  # type: ignore  # noqa F405
